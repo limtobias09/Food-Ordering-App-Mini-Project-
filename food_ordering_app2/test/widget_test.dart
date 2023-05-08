9768a6e96fdf31d5616f254f5008942fb77d4112
limtobias09 <tobiasssalim@stuents.unnes.ac.id>
2023-05-08 20:22:02 +0700

    UI Testing untuk Home Screen

--- food_ordering_app2/test/widget_test.dart
+++ food_ordering_app2/test/widget_test.dart
@@ -1,30 +1,23 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:food_ordering_app2/view/home/home_screen.dart';
 
-import 'package:food_ordering_app2/main.dart';
+
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Test UI di Home Screen', (WidgetTester tester) async{
+    await tester.pumpWidget(const MaterialApp(
+      home: HomeScreen(),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    //Test app bar title text
+    expect(find.text('What Would You Like To Eat Today?'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    //Test app bar background color
+    final findAppbar=find.byType(AppBar);
+    final appBarWidget= tester.widget<AppBar>(findAppbar);
+    expect(appBarWidget.backgroundColor, equals(const Color.fromARGB(255, 154, 41, 33)));
+     
   });
 }
+
