3b64379428d9cfc5933f20b3b83d7f9846a0382a
limtobias09 <tobiasssalim@stuents.unnes.ac.id>
2023-05-11 21:02:54 +0700

    Menambahkan Unit Testing Yang Lain

--- food_ordering_app2/test/unit_test.dart
+++ food_ordering_app2/test/unit_test.dart
@@ -1,37 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-import 'package:food_ordering_app2/view/description_save_order/description_screen.dart';
+import 'package:food_ordering_app2/model/controller/cart_controller.dart';
 
 void main() {
-  testWidgets('Test increment and decrement count', (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(
-      home: DescriptionScreen(),
-    ));
-
-    //Cari tombol increment dan decrement
-    final incrementButton = find.byIcon(Icons.add);
-    final decrementButton = find.byIcon(Icons.remove);
-
-    //Cari text untuk menampilkan angka hasil dari increment dan decrement
-    final countText = find.byType(Text);
-
-    //Memverifikasi bahwa nilai awal-awal adalah 0
-    expect(countText, findsOneWidget);
-    expect((tester.widget(countText) as Text).data, '0');
-
-    //Test saat pencet tombol increment akan berhasil bertambah menjadi angka 1
-    await tester.tap(incrementButton);
-    await tester.pump();
-    expect((tester.widget(countText) as Text).data, '1');
-
-    //Test saat pencet tombol decrement akan berhasil berkurang nilainya
-    await tester.tap(decrementButton);
-    await tester.pump();
-    expect((tester.widget(countText) as Text).data, '0');
-
-    //Memverifikasi bahwa angkanya tidak kurang dari 0
-    await tester.tap(decrementButton);
-    await tester.pump();
-    expect((tester.widget(countText) as Text).data, '0');
+  group('CartController', () {
+    test('addToCart should add new item to cartItems', () {
+      final controller = CartController();
+      controller.addToCart('Jamur Kuping Nanas Cah Bakso', "assets/images/menuoftheday3.jpg", 2);
+      expect(controller.cartItems.length, equals(1));
+      expect(controller.cartItems[0].title, equals('Jamur Kuping Nanas Cah Bakso'));
+      expect(controller.cartItems[0].quantity, equals(2));
+    });  
   });
 }
